e73803c4971d8c40f0fd4d6b83f4610dd596e060
Vitor <vfarias@bionexo.com>
2021-11-13 00:13:04 -0400

    Add

---
+++ Initialize.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Help_NewtonSoft
+{
+    public static class Initialize
+    {
+        public static List<Order> InitOrders()
+        {
+            return new List<Order>()
+            {
+                new Order(1, DateTime.Now, InitCustomer().First(), InitProducts().Take(2).ToList()),
+                new Order(1, DateTime.Now, InitCustomer().Last(), InitProducts().Skip(2).Take(1).ToList()),
+            };
+        }
+
+        private static List<Product> InitProducts()
+        {
+            return new List<Product>()
+            {
+                new Product(1, "Leite", 4.50m, true),
+                new Product(2, "Manteiga", 17.00m, false),
+                new Product(3, "Biscoito", 2.70m, true),
+            };
+        }
+
+        private static List<Customer> InitCustomer()
+        {
+            return new List<Customer>()
+            {
+                new Customer(1, "Vitor", new Address(1, "Rua Tucurui", 3998)),
+                new Customer(2, "Cristinae", new Address(2, "Rua Geraldo Siqueira", 2680))
+            };
+        }
+
+    }
+
+    public class Address
+    {
+        public Address(int id, string street, int number)
+        {
+            Id = id;
+            Street = street;
+            Number = number;
+        }
+
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+    }
+
+    public class Customer
+    {
+        public Customer(int id, string name, Address address)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Product
+    {
+        public Product(int id, string name, decimal price, bool active)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Active = active;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public bool Active { get; set; }
+    }
+
+    public class Order
+    {
+        public Order(int id, DateTime date, Customer customer, List<Product> products)
+        {
+            Id = id;
+            Date = date;
+            Customer = customer;
+            Products = products;
+        }
+
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public Customer Customer { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Help_NewtonSoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnonymousType();
+        }
+
+        private static void Process()
+        {
+            List<Order> orders = Initialize.InitOrders();
+
+            string json = JsonConvert.SerializeObject(orders); 
+
+            orders = JsonConvert.DeserializeObject<List<Order>>(json);
+            
+            // Se não quiser criar classes para deserializar, e ainda trabalhar Linq C#:
+            JToken jToken = JToken.Parse(json);
+            JToken primeiraOrdem = jToken[0];
+            // or:
+            primeiraOrdem = jToken.First();
+            JToken nomeCliente = jToken.First()["Customer"]["Name"];
+            string nomeRuaClienteMora = jToken.First()["Customer"]["Address"]["Street"].ToString();
+
+
+            string jsonIdented = JsonConvert.SerializeObject(orders, Formatting.Indented);
+            Console.WriteLine(jsonIdented);
+
+        }
+
+        private static void PrintIdented()
+        {
+            List<Order> orders = Initialize.InitOrders();
+
+            string json = JsonConvert.SerializeObject(orders, Formatting.Indented); 
+            Console.WriteLine(json);
+        }
+
+        private static void WriterFile()
+        {
+            List<Order> orders = Initialize.InitOrders();
+            File.WriteAllText(@"./orders.json", JsonConvert.SerializeObject(orders));
+            
+            using (StreamWriter file = File.CreateText(@"./orders.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, orders);
+            }
+        }
+
+        private static void AnonymousType()
+        {
+            var definition = new { Name = "" };
+
+            string json1 = @"{'Name':'James'}";
+            var customer1 = JsonConvert.DeserializeAnonymousType(json1, definition);
+
+            Console.WriteLine(customer1.Name);
+        }
+        
+    }
+}
